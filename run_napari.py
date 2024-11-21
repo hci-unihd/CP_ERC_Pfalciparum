feca5ad2f89c8937c620e32e54dd3efb39167929
Sebastian Damrich <sebastian.damrich@gmx.de>
2024-11-21 17:33:44 +0100

    update napari script to new handling of LayerDataTuple

--- run_napari.py
+++ run_napari.py
@@ -5,42 +5,30 @@ from napari.types import LayerDataTuple
 import numpy as np
 from typing import List
 
-
-# custom napari function that splits channels
+# Custom napari function that splits channels
 @magicgui(call_button="Split channels")
-def split(viewer: napari.Viewer, layer: Layer) -> List[LayerDataTuple]:
+def split(viewer: napari.Viewer, layer: Layer) -> None:
     image = layer.data
     name = layer.name
-    del viewer.layers[name]  # delete old layer
+    del viewer.layers[name]  # Delete the old layer
 
+    # Determine the channel dimension (assume smallest shape for channels)
     channel_dim = np.argmin(image.shape)
     image = np.moveaxis(image, channel_dim, 1)
     n_channels = image.shape[1]
 
-    # first channel is always raw
+    # First channel is always raw
     raw = image[:, 0]
 
-    return_object = [
-        (raw,
-         dict(name='raw', opacity=0.5, scale=[3.2, 1, 1]),
-         'image'),
-    ]
+    # Add the "raw" layer
+    viewer.add_image(raw, name='raw', opacity=0.5, scale=[3.2, 1, 1])
 
-    # subsequent channels are all segmentation channels, give them generic names
-    for i in range(n_channels-1):
-        seg = image[:, i+1]
-        return_object.append((
-            seg.astype('uint16'),
-            dict(name=f'seg{i+1}',
-                 opacity=0.5,
-                 scale=[3.2, 1, 1],
-                 ),
-            'labels'),
-        )
-    return return_object
+    # Add segmentation channels with generic names
+    for i in range(1, n_channels):
+        seg = image[:, i]
+        viewer.add_labels(seg.astype('uint16'), name=f'seg{i}', opacity=0.5, scale=[3.2, 1, 1])
 
-
-# add widget and fire up napari
+# Add widget and fire up napari
 viewer = napari.Viewer()
 viewer.window.add_dock_widget(split)
 napari.run()
